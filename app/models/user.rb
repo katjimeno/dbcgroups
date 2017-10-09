9a10b88b21f61d548e0f7eae3ebbbddbac9ffec7
Kathryn Ragudos <kragudos19@gmail.com>
2017-10-08 17:21:20 -0700

    add user function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # , :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  class << self
+    def serialize_from_session(key,salt)                              
+      record = to_adapter.get(key.to_s)
+      record if record && record.authenticatable_salt == salt
+    end
+  end
+
 end
