9a20373e102af52b8aae921269fac8a343415008
Kathryn Ragudos <kragudos19@gmail.com>
2017-10-08 20:59:28 -0700

    route user_root_url

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   # redirect_to authenticated_root_path
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    if current_user
+      return redirect_to(user_root_url)
+    else
+      return "else"
+    end
+    super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
