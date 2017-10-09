e66bbdd92159f102f78384ba40437b733468cc39
Kathryn Ragudos <kragudos19@gmail.com>
2017-10-08 21:21:25 -0700

    after sign in function in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+ def after_sign_in_path_for(resource)   
+    @user = current_user 
+    #  if current_user.admin?     
+    #   stored_location_for(resource) || admin_path     
+    # else       
+    #    stored_location_for(resource) || dashboard_path(current_user.id)      
+    #  end       
+  end     
 end
